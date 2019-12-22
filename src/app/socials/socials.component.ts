becff3b3d3a065fa6778693ec4054dd916b7f91c
Dorokhovich Mikhail <mikhail@dorokhovich.ru>
2019-12-22 20:19:39 +0300

    added scaffloded component for header

---
+++ src/app/socials/socials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ba-socials',
+  templateUrl: './socials.component.html',
+  styleUrls: ['./socials.component.css']
+})
+export class SocialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
