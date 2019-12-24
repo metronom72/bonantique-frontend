110aea7a1568b7f7919163e08c8a2dc5f9aa04e8
Your Name <you@example.com>
2019-12-24 05:41:16 -0800

    styled banner

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'ba-banner',
@@ -7,16 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
-  public type: 'VERTICAL' | 'HORIZONTAL';
-  public header: string | null;
-  public subheader: string | null;
-  public raw: string | null;
-  public frontImage: string | null;
-  public link: string;
+  public type: 'VERTICAL' | 'HORIZONTAL' = 'HORIZONTAL';
+  public header: string | null = 'Распродажа частной коллекции';
+  public subheader: string | null = 'Первая в России распродажа';
+  public raw: string | null = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+  public frontImage: string | null = 'https://i.pinimg.com/564x/9e/28/25/9e28256b8a0937795d2dc94916b14666.jpg';
+  public link: string | null = '#';
   private id: string;
 
-  constructor() { }
+  public isMobile: boolean;
 
-  ngOnInit() { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(['(max-width: 820px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.isMobile = true;
+        } else {
+          this.isMobile = false;
+        }
+      });
+  }
 
 }
