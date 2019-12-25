e4680f4d6aa99eb53ffc1d782e7b1f392e6b67c9
Your Name <you@example.com>
2019-12-25 05:38:37 -0800

    added common breadcrumbs

--- src/app/breadcrumbs/breadcrumbs.component.ts
+++ src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ba-breadcrumbs',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
+  @Input()
+  paths: string[] = []
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
