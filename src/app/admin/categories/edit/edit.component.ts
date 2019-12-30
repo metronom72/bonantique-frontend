1e8b2e19004ab6780c426feb0c6c852abc2bc5bc
Your Name <you@example.com>
2019-12-30 04:46:37 -0800

    fixed issues with redirection in category form

--- src/app/admin/categories/edit/edit.component.ts
+++ src/app/admin/categories/edit/edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../../../common/services/categories.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Category } from '../../../common/category';
 
 @Component({
@@ -27,24 +27,35 @@ export class EditComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.init(this.categories);
+      }
+    });
     this.categoriesService.categories
-      .subscribe(
-        (categories: Category[]) => {
-          this.categories = categories;
-          if (this.route.snapshot.params.slug === 'new') {
-            this.type = 'new';
-          } else {
-            this.type = 'edit';
+      .subscribe(this.init);
+  }
 
-            const category = categories.find(c => c.slug === this.route.snapshot.params.slug);
-            if (category) {
-              this.categoryForm.reset(category);
-            } else {
-              this.router.navigateByUrl('/admin');
-            }
-          }
-        }
-      );
+  private init = (categories: Category[]) => {
+    console.log(categories);
+    this.categories = categories;
+    if (this.route.snapshot.params.slug === 'new') {
+      this.type = 'new';
+      this.categoryForm.reset({
+        title: '',
+        slug: '',
+        active: true,
+      });
+    } else {
+      this.type = 'edit';
+
+      const category = categories.find(c => c.slug === this.route.snapshot.params.slug);
+      if (category) {
+        this.categoryForm.reset(category);
+      } else {
+        this.router.navigateByUrl('/admin');
+      }
+    }
   }
 
   onSubmit = () => {
@@ -53,7 +64,12 @@ export class EditComponent implements OnInit {
       this.categoriesService.updateCategory(category.slug, this.categoryForm.value)
         .subscribe(
           (values: any) => {
-            this.categoriesService.getCategories();
+            this.categoriesService.getCategories()
+              .subscribe(
+                (vals: { data: Category[] }) => {
+                  this.categoriesService.categories.next(vals.data);
+                }
+              );
           },
           (errors: any) => {
             // pass
@@ -63,7 +79,12 @@ export class EditComponent implements OnInit {
       this.categoriesService.createCategory(this.categoryForm.value)
         .subscribe(
           (values: any) => {
-            this.categoriesService.getCategories();
+            this.categoriesService.getCategories()
+              .subscribe(
+                (vals: { data: Category[] }) => {
+                  this.categoriesService.categories.next(vals.data);
+                }
+              );;
           },
           (errors: any) => {
             // pass
